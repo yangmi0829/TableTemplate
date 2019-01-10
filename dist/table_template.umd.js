282ba2dd03bf67cea36a26383cd7b84150a1f063
ym <yangmi0829@vip.qq.com>
2019-01-10 10:14:24 +0800

    增加readme

---
+++ dist/table_template.umd.js
@@ -0,0 +1,381 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["table_template"] = factory();
+	else
+		root["table_template"] = factory();
+})((typeof self !== 'undefined' ? self : this), function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "fae3");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "fae3":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/@vue/cli-service/lib/commands/build/setPublicPath.js
+// This file is imported into lib/wc client bundles.
+
+if (typeof window !== 'undefined') {
+  var i
+  if ((i = window.document.currentScript) && (i = i.src.match(/(.+\/)[^/]+\.js(\?.*)?$/))) {
+    __webpack_require__.p = i[1] // eslint-disable-line
+  }
+}
+
+// Indicate to webpack that this file can be concatenated
+/* harmony default export */ var setPublicPath = (null);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"1f9db21e-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/table/TableTemplate.vue?vue&type=template&id=4c2dd4fa&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('el-table',{staticStyle:{"width":"100%"},attrs:{"span-method":_vm.spanMethod,"data":_vm.tableData,"border":"","stripe":""},on:{"row-click":_vm.handleRowClick,"cell-click":_vm.handleCellClick,"selection-change":_vm.handleSelectionChange}},[_vm._l((_vm.headers),function(i,$index){return [_c('table-column',{key:("" + $index),attrs:{"col":i},scopedSlots:_vm._u([{key:i.slot,fn:function(ref){
+var scope = ref.scope;
+return (i.slot)?[_vm._t(i.slot,[_vm._v(_vm._s(i.slot))],{scope:scope})]:undefined}}])})]})],2)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/table/TableTemplate.vue?vue&type=template&id=4c2dd4fa&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"1f9db21e-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/table/TableColumn.vue?vue&type=template&id=734292a1&scoped=true&
+var TableColumnvue_type_template_id_734292a1_scoped_true_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('transition',[(_vm.col.type == 'selection')?_c('el-table-column',{attrs:{"type":"selection","label":"","width":"55"}}):(_vm.col.type == 'index')?_c('el-table-column',{attrs:{"type":"index","label":"序号"}}):(_vm.col.type == 'expand')?_c('el-table-column',{attrs:{"type":"expand"},scopedSlots:_vm._u([{key:"default",fn:function(scope){return [_vm._t(_vm.col.slot,null,{scope:scope})]}}])}):_c('el-table-column',{attrs:{"prop":_vm.col.prop,"show-overflow-tooltip":_vm.col.tips,"align":_vm.col.center || 'center',"label":_vm.col.label,"width":_vm.col.width,"min-width":_vm.col.minWidth,"fixed":_vm.col.fixed},scopedSlots:_vm._u([{key:"default",fn:function(scope){return (!_vm.col.children)?[(!_vm.col.slot)?[_vm._v(_vm._s(scope.row[_vm.col.prop]))]:_vm._t(_vm.col.slot,null,{scope:scope})]:undefined}}])},[_vm._l((_vm.col.children),function(children,$index){return [_c('table-column',{key:("" + $index),attrs:{"col":children},scopedSlots:_vm._u([{key:children.slot,fn:function(ref){
+var scope = ref.scope;
+return (children.slot)?[_vm._t(children.slot,null,{scope:scope})]:undefined}}])})]})],2)],1)}
+var TableColumnvue_type_template_id_734292a1_scoped_true_staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/table/TableColumn.vue?vue&type=template&id=734292a1&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/table/TableColumn.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var TableColumnvue_type_script_lang_js_ = ({
+  name: 'table-column',
+  props: {
+    col: {
+      type: Object,
+      required: true
+    }
+  }
+});
+// CONCATENATED MODULE: ./src/components/table/TableColumn.vue?vue&type=script&lang=js&
+ /* harmony default export */ var table_TableColumnvue_type_script_lang_js_ = (TableColumnvue_type_script_lang_js_); 
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+/* globals __VUE_SSR_CONTEXT__ */
+
+// IMPORTANT: Do NOT use ES2015 features in this file (except for modules).
+// This module is a runtime utility for cleaner component module output and will
+// be included in the final webpack user bundle.
+
+function normalizeComponent (
+  scriptExports,
+  render,
+  staticRenderFns,
+  functionalTemplate,
+  injectStyles,
+  scopeId,
+  moduleIdentifier, /* server only */
+  shadowMode /* vue-cli only */
+) {
+  // Vue.extend constructor export interop
+  var options = typeof scriptExports === 'function'
+    ? scriptExports.options
+    : scriptExports
+
+  // render functions
+  if (render) {
+    options.render = render
+    options.staticRenderFns = staticRenderFns
+    options._compiled = true
+  }
+
+  // functional template
+  if (functionalTemplate) {
+    options.functional = true
+  }
+
+  // scopedId
+  if (scopeId) {
+    options._scopeId = 'data-v-' + scopeId
+  }
+
+  var hook
+  if (moduleIdentifier) { // server build
+    hook = function (context) {
+      // 2.3 injection
+      context =
+        context || // cached call
+        (this.$vnode && this.$vnode.ssrContext) || // stateful
+        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
+      // 2.2 with runInNewContext: true
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__
+      }
+      // inject component styles
+      if (injectStyles) {
+        injectStyles.call(this, context)
+      }
+      // register component module identifier for async chunk inferrence
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier)
+      }
+    }
+    // used by ssr in case component is cached and beforeCreate
+    // never gets called
+    options._ssrRegister = hook
+  } else if (injectStyles) {
+    hook = shadowMode
+      ? function () { injectStyles.call(this, this.$root.$options.shadowRoot) }
+      : injectStyles
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // for template-only hot-reload because in that case the render fn doesn't
+      // go through the normalizer
+      options._injectStyles = hook
+      // register for functioal component in vue file
+      var originalRender = options.render
+      options.render = function renderWithStyleInjection (h, context) {
+        hook.call(context)
+        return originalRender(h, context)
+      }
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate
+      options.beforeCreate = existing
+        ? [].concat(existing, hook)
+        : [hook]
+    }
+  }
+
+  return {
+    exports: scriptExports,
+    options: options
+  }
+}
+
+// CONCATENATED MODULE: ./src/components/table/TableColumn.vue
+
+
+
+
+
+/* normalize component */
+
+var component = normalizeComponent(
+  table_TableColumnvue_type_script_lang_js_,
+  TableColumnvue_type_template_id_734292a1_scoped_true_render,
+  TableColumnvue_type_template_id_734292a1_scoped_true_staticRenderFns,
+  false,
+  null,
+  "734292a1",
+  null
+  
+)
+
+component.options.__file = "TableColumn.vue"
+/* harmony default export */ var TableColumn = (component.exports);
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/table/TableTemplate.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ var TableTemplatevue_type_script_lang_js_ = ({
+  name: 'table-Template',
+  props: {
+    spanMethod: {
+      type: Function
+    },
+    headers: {
+      type: Array,
+      required: true
+    },
+    tableData: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    TableColumn: TableColumn
+  },
+  methods: {
+    handleSelectionChange: function handleSelectionChange(val) {
+      this.$emit('selectionChange', val);
+    },
+    handleRowClick: function handleRowClick(row, event, column) {
+      this.$emit('handleRowClick', row, event, column);
+    },
+    handleCellClick: function handleCellClick(row, column, cell, event) {
+      this.$emit('handleCellClick', row, column, cell, event);
+    }
+  }
+});
+// CONCATENATED MODULE: ./src/components/table/TableTemplate.vue?vue&type=script&lang=js&
+ /* harmony default export */ var table_TableTemplatevue_type_script_lang_js_ = (TableTemplatevue_type_script_lang_js_); 
+// CONCATENATED MODULE: ./src/components/table/TableTemplate.vue
+
+
+
+
+
+/* normalize component */
+
+var TableTemplate_component = normalizeComponent(
+  table_TableTemplatevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+TableTemplate_component.options.__file = "TableTemplate.vue"
+/* harmony default export */ var TableTemplate = (TableTemplate_component.exports);
+// CONCATENATED MODULE: ./src/index.js
+
+// CONCATENATED MODULE: ./node_modules/@vue/cli-service/lib/commands/build/entry-lib-no-default.js
+
+
+
+
+/***/ })
+
+/******/ });
+});
+//# sourceMappingURL=table_template.umd.js.map
